45abaacb86c4cd7c3d724eb7c6095890b5e32120
wMC0yMDAsMTA <wMC0yMDAsMTA@proton.me>
2024-12-08 21:50:38 +0800

    Update _ct06yuqs

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd5529e87-171a-4d6b-a13c-af1b9db596e9';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
